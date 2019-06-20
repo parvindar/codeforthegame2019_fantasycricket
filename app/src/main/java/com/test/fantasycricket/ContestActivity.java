a7e029cb7ec78ef272682d3e4bdd629b6ffcc3c8
Parvindar Singh <parvindar.email@gmail.com>
2019-06-20 23:59:10 +0530

    created contest class

--- app/src/main/java/com/test/fantasycricket/ContestActivity.java
+++ app/src/main/java/com/test/fantasycricket/ContestActivity.java
@@ -9,10 +9,24 @@ import java.util.List;
 public class ContestActivity extends AppCompatActivity {
     ListView contestlist;
     static String matchid;
+    String team1,team2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contest);
+    }
+
+
+
+    class Contest{
+        public Integer prize,price,spotsfilled,totalspots;
+
+        public Contest(Integer prize, Integer price, Integer spotsfilled, Integer totalspots) {
+            this.prize = prize;
+            this.price = price;
+            this.spotsfilled = spotsfilled;
+            this.totalspots = totalspots;
+        }
     }
 }
